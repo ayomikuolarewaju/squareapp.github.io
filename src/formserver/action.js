763be7911df34517ccd11e2b3668c3b218eca400
Ayomiku Almaroof Olarewaju <53964338+ayomikuolarewaju@users.noreply.github.com>
2025-03-27 07:26:01 +0100

    Add files via upload

--- src/formserver/action.js
+++ src/formserver/action.js
@@ -1,28 +1,25 @@
-"use server"
+"use server";
 
 import axios from "axios";
-import Link from "next/link";
-
-
-
+import { redirect } from "next/navigation";
+redirect;
 
 export async function formServer(prevState, queryData) {
+  const fullname = queryData.get("fullname");
+  const email = queryData.get("email");
+  const phone = queryData.get("phone");
 
-
-    const fullname = queryData.get('fullname');
-    const email = queryData.get('email');
-    const phone = queryData.get('phone');
-
-    if (!fullname && !email && !phone) {
-      return;
-    } else {
-        
-        const poster =  axios.post("http://127.0.0.1:8080/api/user",{
-
-            email: await email,
-            fullname: await fullname,
-            phone: await phone
-        })
-       return `successfuly submitted, get back to you ${await fullname}`
-    }
+  if (!fullname && !email && !phone) {
+    return;
+  } else {
+    const poster = axios.post(
+      "http://ssquare-app-env.eba-t2ymhzv5.us-east-2.elasticbeanstalk.com/api/user",
+      {
+        email: await email,
+        fullname: await fullname,
+        phone: await phone,
+      }
+    );
+    return redirect("/thanks");
   }
+}
